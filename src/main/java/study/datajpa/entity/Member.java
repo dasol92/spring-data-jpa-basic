876105085bad3723e1211735ed87a5a0089bfa3c
Dasol Kim <zoocanon92@gmail.com>
2024-02-04 23:44:05 +0900

    Initial commit

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,25 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String userName;
+
+    protected Member() {
+
+    }
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+
+}
